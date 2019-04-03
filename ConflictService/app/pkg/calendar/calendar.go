714308901a122013591f2dc99f4f07459940d54f
Jon <jon.dinardo@mediasonar.com>
2019-04-02 21:32:53 -0400

    wrote sort event, time for a unit test!

--- ConflictService/app/pkg/calendar/calendar.go
+++ ConflictService/app/pkg/calendar/calendar.go
@@ -2,7 +2,9 @@ package calendar
 
 import (
 	"context"
+	"fmt"
 	protobufs "scheduling"
+	"sort"
 )
 
 // Server represents our implementation of the scheduling interface
@@ -16,5 +18,30 @@ func NewServer() *Server {
 
 // GetConflicts -- (ctx, conflictList) Returns a sequence of events that conflict with eachother
 func (s *Server) GetConflicts(ctx context.Context, eventList *protobufs.EventList) (*protobufs.ConflictList, error) {
-	return &protobufs.ConflictList{}, nil
+
+	conflictPairs := getConflictPairs(eventList)
+
+	return conflictPairs, nil
+}
+
+func getConflictPairs(eventList *protobufs.EventList) *protobufs.ConflictList {
+	//First step, sort them in nlogn time by start time
+	_ = sortEventList(eventList)
+
+	//Next step, just iterate down the events checking to see if the end time of n is at or after the start time of n + 1
+
+	return &protobufs.ConflictList{}
+}
+
+func sortEventList(eventList *protobufs.EventList) []*protobufs.Event {
+	events := eventList.Events
+
+	// Go's built in sort, sort's in nLogn time..
+	sort.Slice(events, func(a, b int) bool {
+		return events[a].Start.Nanos < events[b].Start.Nanos
+	})
+
+	fmt.Println(events)
+
+	return events
 }
