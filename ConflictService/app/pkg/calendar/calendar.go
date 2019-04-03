71d1e32609269b6c699461164e16b9949b092236
Jon <jon.dinardo@mediasonar.com>
2019-04-02 20:58:25 -0400

    created basic stubbed out implementation

---
+++ ConflictService/app/pkg/calendar/calendar.go
@@ -0,0 +1,19 @@
+package calendar
+
+import (
+	protobufs "scheduling"
+)
+
+// Server represents our implementation of the scheduling interface
+type Server struct {
+}
+
+// NewServer -- (): Create a new server that implements the ConflictService rpc server interface
+func NewServer() *Server {
+	return &Server{}
+}
+
+// GetConflicts -- (ctx, conflictList) Returns a sequence of events that conflict with eachother
+func (s *Server) GetConflicts(ctx context.Context, eventList *protobufs.EventList) (*protobufs.ConflictList, error) {
+	return &protobufs.ConflictList{}, nil
+}
